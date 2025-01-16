48024381b47e79bbd31edf00c2562e75b7a081d6
fayinana <fekedeananiya@gmail.com>
2025-01-16 12:49:50 +0300

    test the online DB

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
-// const DB = process.env.DATABASE.replace(
-//   "<PASSWORD>",
-//   process.env.DATABASE_PASSWORD
-// );
-const DB = process.env.LOCAL_DB;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+// const DB = process.env.LOCAL_DB;
 
 mongoose.connect(DB).then((db) => {
   console.log(`db connected successfully ${db.connection.name}`);
